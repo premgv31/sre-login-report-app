8758d79c0007511fb306cc0d8129256727a33e1c
premgv31 <civilianprem@gmail.com>
2025-06-20 12:58:19 +0530

    fixed main

--- app/main.py
+++ app/main.py
@@ -68,3 +68,6 @@ def get_report(token: str):
     username = verify_token(token)
     return {"report": f"Hello {username}, here is your dummy report data."}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
